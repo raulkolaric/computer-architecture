ec5cd3938730cafa4a214db4b234f91b58e35a95
raulkolaric <raul.kolaric@gmail.com>
2026-04-06 08:04:57 -0300

    add: answers lab04 - Rodrigo

--- lab04-procedures-sorting/ex1.asm
+++ lab04-procedures-sorting/ex1.asm
@@ -1,1 +1,22 @@
-.
+#Igor Gabriel Santos Simões RA00360505
+#Raul Kolaric RA00359586
+#Rodrigo Ward Leite RA00359800
+
+addi $t0, $zero, 890    # t0 = 890 
+add $a0, $zero, $t0   # a0 = t0 = 890
+
+jal eMaiorQue100            # JAL (jump and link) chama a funcao "eMaiorQue100"
+add $t5, $zero, $v0   # Move o valor de retorno, armazenado em v0 para t5
+j fim		      # Pula pro label "fim"
+
+# ====================================== Funcao ================================================
+eMaiorQue100:
+    slti $a1, $a0, 101 # realiza a expressao ternaria a1 = a0 < 100 ? 1 : 0
+    beq $a1, $zero, maiorQue100 # if a1 == 0 goto maiorQue100
+    addi $v0, $zero, 100    # v0 = $zero + 100
+    jr $ra # Retorno da funcao
+    maiorQue100: addi $v0, $zero, 999 # v0 = $zero + 999
+    jr $ra # Retorno da funcao
+# =============================================================================================
+
+fim:
